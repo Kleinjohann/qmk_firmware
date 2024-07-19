7d2cc76269cc1b755bf6ad64df79668cf2782102
Alexander Kleinjohann <kleinjohann@posteo.de>
2024-07-19 22:27:57 +0200

    Switch to US Intl and revive dead keys
    add third layer with one time switch for umlaut etc
    automatically append space to all dead keys

--- keyboards/crkbd/keymaps/kleinjohann/config.h
+++ keyboards/crkbd/keymaps/kleinjohann/config.h
@@ -34,23 +34,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LSPO_KEYS KC_LSFT, KC_TRNS, KC_SPC
 #define RSPC_KEYS KC_RSFT, KC_TRNS, KC_SPC
 #define LAPO_KEYS KC_LALT, KC_TRNS, KC_ESC
-#define LCPO_KEYS KC_LCTL, KC_TRNS, KC_ENT
 
-#define COMBO_COUNT 11
+// Keycodes for umlauts and eszett
+#define UML_AE RALT(KC_Q)
+#define UML_OE RALT(KC_P)
+#define UML_UE RALT(KC_Y)
+#define GER_SZ RALT(KC_S)
+#define EU_EUR RALT(KC_5)
+#define GB_PND SAGR(KC_4)
+#define SI_MU  RALT(KC_M)
 
-#ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_KNIGHT
-    #define RGBLIGHT_EFFECT_CHRISTMAS
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RGB_TEST
-    #define RGBLIGHT_EFFECT_ALTERNATING
-    #define RGBLIGHT_EFFECT_TWINKLE
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
+#define COMBO_COUNT 14
+
--- keyboards/crkbd/keymaps/kleinjohann/keymap.c
+++ keyboards/crkbd/keymaps/kleinjohann/keymap.c
@@ -18,6 +18,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+// Keycodes for dead keys
+enum custom_keycodes {
+    MC_QUOT = SAFE_RANGE,
+    MC_DQT,
+    MC_GRV,
+    MC_TILD,
+    MC_OSL2
+};
+
+#define LCTL_OSL_LAYER  2
+#define LCTL_OSL  LCTL_T(MC_OSL2 /*placeholder*/)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_3(
   //,--------------------------------------------.                    ,--------------------------------------------.
@@ -27,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
-                                 KC_LGUI, SC_LAPO, SC_LSPO,    SC_RSPC,   MO(1), SC_LCPO
+                                 KC_LGUI, SC_LAPO, SC_LSPO,    SC_RSPC,   MO(1), LCTL_OSL
                              //`--------------------------'  `--------------------------'
 
   ),
@@ -36,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                    ,--------------------------------------------.
          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-       KC_GRV,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_BSLS,
+       MC_GRV,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_BSLS,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_UNDS, KC_PLUS, KC_MINS,  KC_EQL, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
@@ -44,69 +57,102 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              //`--------------------------'  `--------------------------'
   ),
 
-  /*  [2] = LAYOUT_split_3x5_3(*/
-  /*//,--------------------------------------------.                    ,--------------------------------------------.*/
-  /*    KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,*/
-  /*//|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|*/
-  /*    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,*/
-  /*//|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|*/
-  /*    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,*/
-  /*//|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|*/
-  /*                               KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT*/
-  /*                           //`--------------------------'  `--------------------------'*/
-  /*),*/
-  /**/
-  /*  [3] = LAYOUT_split_3x5_3(*/
-  /*//,--------------------------------------------.                    ,--------------------------------------------.*/
-  /*    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,*/
-  /*//|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|*/
-  /*    RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,*/
-  /*//|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|*/
-  /*    RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,*/
-  /*//|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|*/
-  /*                               KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT*/
-  /*                           //`--------------------------'  `--------------------------'*/
-  /*)*/
+    [2] = LAYOUT_split_3x5_3(
+  //,--------------------------------------------.                    ,--------------------------------------------.
+      KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC,  UML_UE, KC_ASTR,  UML_OE,  GB_PND,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+       UML_AE,  GER_SZ,  EU_EUR, _______, _______,                      MC_TILD, KC_AMPR, MC_QUOT, MC_DQT,  _______,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+      _______, _______, _______, _______, _______,                      _______,   SI_MU, _______, _______, _______,
+  //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
+                                 KC_LGUI, SC_LAPO, SC_LSPO,    SC_RSPC,   MO(1),   TO(0)
+                             //`--------------------------'  `--------------------------'
+
+  ),
+
 };
 
 enum combos {
-  S_D_TAB,
-  K_L_ESC,
-  R_T_9,
-  Y_U_0,
-  F_G_LBRC,
-  H_J_RBRC,
-  J_K_ENT,
-  V_B_LCBR,
-  M_N_RCBR,
-  COMM_DOT_QUOT,
-  I_O_BSPC,
+    W_E_ESC,
+    E_R_ESC,
+    S_D_TAB,
+    D_F_TAB,
+    R_T_9,
+    Y_U_0,
+    F_G_LBRC,
+    H_J_RBRC,
+    J_K_ENT,
+    K_L_ENT,
+    V_B_LCBR,
+    M_N_RCBR,
+    COMM_DOT_QUOT,
+    I_O_BSPC,
 
 };
 
-const uint16_t PROGMEM s_d_tab[] = { KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM k_l_esc[] = { KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM r_t_9[] = { KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM y_u_0[] = { KC_Y, KC_U, COMBO_END};
-const uint16_t PROGMEM f_g_lbrc[] = { KC_F, KC_G, COMBO_END};
-const uint16_t PROGMEM h_j_rbrc[] = { KC_H, KC_J, COMBO_END};
-const uint16_t PROGMEM j_k_ent[] = { KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM v_b_lcbr[] = { KC_V, KC_B, COMBO_END};
-const uint16_t PROGMEM m_n_rcbr[] = { KC_M, KC_N, COMBO_END};
-const uint16_t PROGMEM comm_dot_quot[] = { KC_COMM, KC_DOT, COMBO_END};
-const uint16_t PROGMEM i_o_bspc[] = { KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM w_e_esc[]       = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM e_r_esc[]       = {KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM s_d_tab[]       = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM d_f_tab[]       = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM r_t_9[]         = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM y_u_0[]         = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM f_g_lbrc[]      = {KC_F, KC_G, COMBO_END};
+const uint16_t PROGMEM h_j_rbrc[]      = {KC_H, KC_J, COMBO_END};
+const uint16_t PROGMEM j_k_ent[]       = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM k_l_ent[]       = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM v_b_lcbr[]      = {KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM m_n_rcbr[]      = {KC_M, KC_N, COMBO_END};
+const uint16_t PROGMEM comm_dot_quot[] = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM i_o_bspc[]      = {KC_I, KC_O, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
+  [W_E_ESC] = COMBO(w_e_esc, KC_ESC),
+  [E_R_ESC] = COMBO(e_r_esc, KC_ESC),
   [S_D_TAB] = COMBO(s_d_tab, KC_TAB),
-  [K_L_ESC] = COMBO(k_l_esc, KC_ESC),
+  [D_F_TAB] = COMBO(d_f_tab, KC_TAB),
   [R_T_9] = COMBO(r_t_9, KC_LPRN),
   [Y_U_0] = COMBO(y_u_0, KC_RPRN),
   [F_G_LBRC] = COMBO(f_g_lbrc, KC_LBRC),
   [H_J_RBRC] = COMBO(h_j_rbrc, KC_RBRC),
   [J_K_ENT] = COMBO(j_k_ent, KC_ENT),
+  [K_L_ENT] = COMBO(k_l_ent, KC_ENT),
   [V_B_LCBR] = COMBO(v_b_lcbr, KC_LCBR),
   [M_N_RCBR] = COMBO(m_n_rcbr, KC_RCBR),
-  [COMM_DOT_QUOT] = COMBO(comm_dot_quot, KC_QUOT),
+  [COMM_DOT_QUOT] = COMBO(comm_dot_quot, MC_QUOT),
   [I_O_BSPC] = COMBO(i_o_bspc, KC_BSPC),
 
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MC_QUOT:
+            if (record->event.pressed) {
+                SEND_STRING("'" SS_TAP(X_SPC));
+            }
+            break;
+        case MC_DQT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LSFT("'") SS_TAP(X_SPC));
+            }
+            break;
+        case MC_GRV:
+            if (record->event.pressed) {
+                SEND_STRING("`" SS_TAP(X_SPC));
+            }
+            break;
+        case MC_TILD:
+            if (record->event.pressed) {
+                SEND_STRING("~" SS_TAP(X_SPC));
+            }
+            break;
+        case LCTL_OSL:
+            if (record->tap.count > 0) { // Replace tap behavior with OSL.
+                if (!record->event.pressed) {
+                    set_oneshot_layer(LCTL_OSL_LAYER, ONESHOT_OTHER_KEY_PRESSED);
+                }
+                return false;
+            }
+            break;
+    }
+    return true;
 };
